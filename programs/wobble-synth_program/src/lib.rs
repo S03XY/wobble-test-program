0cdeef782fb15e896a87074a9c06d1266b990980
CryptoGenesis <prishankofficial@gmail.com>
2022-05-20 01:49:41 +0530

    final program

--- programs/wobble-synth_program/src/lib.rs
+++ programs/wobble-synth_program/src/lib.rs
@@ -1,22 +1,97 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke;
+use anchor_spl::token::{Mint, Token, TokenAccount};
+use spl_associated_token_account::get_associated_token_address;
+use spl_token::instruction::transfer_checked;
+use spl_token::ui_amount_to_amount;
+use std::str::FromStr;
 
-declare_id!("54mzQZxsFhqshuPkQhgHU8EBKTnLMtdv51Lpx8Cj4YMB");
+declare_id!("8ptgXekj3hdRqdXvrW8cr55ZoMNLeyqa7Xp13XAAUMBQ");
+
+// CHANGE THIS FOR RECEIVER WALLET
+const RECEIVER_WALLET: &str = "8WiSJ8Z92Rc6SMnbFNV5GdSr1YTnFP9gQ7NwCjhgYKJE";
+const BUYER_PREFIX: &str = "buyer";
 
 #[program]
 pub mod wobble_synth {
+
+
+
     use super::*;
 
     pub fn buy_first_song(ctx: Context<CreateUser0>) -> Result<()> {
         let user_0 = &mut ctx.accounts.user0;
+        let token_mint = &mut ctx.accounts.tokenmint;
+        let user = &mut ctx.accounts.user;
+        let source = &mut ctx.accounts.source;
+        let destination = &mut ctx.accounts.destination;
+        let token_program_id = &mut ctx.accounts.token_program_id;
+
+        let receiver_pubkey = Pubkey::from_str(RECEIVER_WALLET).unwrap();
+        let receiver_ata = get_associated_token_address(&receiver_pubkey, &token_mint.key());
+        let formatted_amount = ui_amount_to_amount(8.0, token_mint.decimals);
+
+        let trasnferins = transfer_checked(
+            &token_program_id.key(),
+            &source.key(),
+            &token_mint.key(),
+            &receiver_ata,
+            &user.key(),
+            &[&user.key()],
+            formatted_amount,
+            token_mint.decimals,
+        )?;
+
+        invoke(
+            &trasnferins,
+            &[
+                token_program_id.to_account_info(),
+                token_mint.to_account_info(),
+                destination.to_account_info(),
+                source.to_account_info(),
+                user.to_account_info(),
+            ],
+        )?;
+
+        user_0.wallet = user.key();
         user_0.songs_count = 1;
 
-        let author = &ctx.accounts.user;
-        user_0.wallet = *author.key;
         Ok(())
     }
 
     pub fn buy_song(ctx: Context<UpdateUser0>) -> Result<()> {
         let user_0 = &mut ctx.accounts.user0;
+        let source = &mut ctx.accounts.source;
+        let destination = &mut ctx.accounts.destination;
+        let token_program_id = &mut ctx.accounts.token_program_id;
+        let token_mint = &mut ctx.accounts.tokenmint;
+        let receiver_pubkey = Pubkey::from_str(RECEIVER_WALLET).unwrap();
+        let receiver_ata = get_associated_token_address(&receiver_pubkey, &token_mint.key());
+        let formatted_amount = ui_amount_to_amount(8.0, token_mint.decimals);
+        let wallet = &mut ctx.accounts.wallet;
+
+        let trasnferins = transfer_checked(
+            &token_program_id.key(),
+            &source.key(),
+            &token_mint.key(),
+            &receiver_ata,
+            &wallet.key(),
+            &[&wallet.key()],
+            formatted_amount,
+            token_mint.decimals,
+        )?;
+
+        invoke(
+            &trasnferins,
+            &[
+                token_program_id.to_account_info(),
+                token_mint.to_account_info(),
+                destination.to_account_info(),
+                source.to_account_info(),
+                wallet.to_account_info(),
+            ],
+        )?;
+
         user_0.songs_count = user_0.songs_count + 1;
         Ok(())
     }
@@ -24,10 +99,17 @@ pub mod wobble_synth {
 
 #[derive(Accounts)]
 pub struct CreateUser0<'info> {
-    #[account(init, payer=user, space=User0::LEN)]
+    #[account(init,payer=user,seeds=[BUYER_PREFIX.as_bytes()],bump, space=User0::LEN)]
     pub user0: Account<'info, User0>,
     #[account(mut)]
     pub user: Signer<'info>,
+    pub tokenmint: Account<'info, Mint>,
+    pub token_program_id: Program<'info, Token>,
+    #[account(mut)]
+    pub source: Account<'info, TokenAccount>,
+    /// CHECK: already checked
+    #[account(mut)]
+    pub destination: UncheckedAccount<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -36,6 +118,13 @@ pub struct UpdateUser0<'info> {
     #[account(mut, has_one=wallet)] // has_one=author => user0.author == UpdateUser0.author.key
     pub user0: Account<'info, User0>,
     pub wallet: Signer<'info>,
+    pub token_program_id: Program<'info, Token>,
+    #[account(mut)]
+    pub source: Account<'info, TokenAccount>,
+    /// CHECK: already checked
+    #[account(mut)]
+    pub destination: UncheckedAccount<'info>,
+    pub tokenmint: Account<'info, Mint>,
 }
 
 #[account]
--- tests/wobble-synth.ts
+++ tests/wobble-synth.ts
@@ -1,65 +1,22 @@
-import * as anchor from "@project-serum/anchor"
-import { Program } from "@project-serum/anchor"
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
 
-import { expect } from "chai"
-import { WobbleSynth } from "../target/types/wobble_synth"
+import { expect } from "chai";
+import { WobbleSynth } from "../target/types/wobble_synth";
 
 describe("wobble-synth_program", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.Provider.env()
-  anchor.setProvider(provider)
-  const user0Account = anchor.web3.Keypair.generate()
-  const user0Program = anchor.workspace.WobbleSynth as Program<WobbleSynth>
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const user0Account = anchor.web3.Keypair.generate();
+  const user0Program = anchor.workspace.WobbleSynth as Program<WobbleSynth>;
 
   it("buy_first_song", async () => {
-    await user0Program.rpc.buyFirstSong({
-      accounts: {
-        user0: user0Account.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      },
-      signers: [user0Account],
-    })
-    const fetchedUser0 = await user0Program.account.user0.fetch(
-      user0Account.publicKey
-    )
-    expect(fetchedUser0.songsCount).to.eq(1)
-  })
+  });
 
   it("buy_song", async () => {
-    const allUsers = await user0Program.account.user0.all([{memcmp: {
-      offset: 8,
-      bytes: provider.wallet.publicKey.toBase58(),
-    }}])
-    const currUser = allUsers[0]
-    await user0Program.rpc.buySong({
-      accounts: {
-        user0: currUser.publicKey,
-        wallet: provider.wallet.publicKey,
-      },
-    })
+    
+  });
 
-    const fetchedUsers = await user0Program.account.user0.fetch(
-      currUser.publicKey
-    )
-    expect(fetchedUsers.songsCount).to.eq(2)
-  })
-
-  it("cannot buy song for other user", async () => {
-    const otherUser = anchor.web3.Keypair.generate()
-    try {
-      await user0Program.rpc.buySong({
-        accounts: {
-          user0: user0Account.publicKey,
-          wallet: otherUser.publicKey,
-        },
-      })
-    } catch (err) {
-      const currUser = await user0Program.account.user0.fetch(
-        user0Account.publicKey
-      )
-      expect(currUser.songsCount).to.eq(2)
-      expect(err.message).to.eq("Signature verification failed")
-    }
-  })
-})
+  it("cannot buy song for other user", async () => {});
+});
